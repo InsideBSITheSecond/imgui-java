df1815568164012b09c14c816913fa07187d94fa
InsideBSI <insidebsiprod@gmail.com>
2025-02-07 06:43:19 +0100

    enable freetype

--- buildSrc/scripts/build.sh
+++ buildSrc/scripts/build.sh
@@ -41,7 +41,7 @@ check_file_exists() {
 case "$VTYPE" in
     windows)
         echo "Running Gradle task for Windows..."
-        ./gradlew imgui-binding:generateLibs -Denvs=windows -Dfreetype=false
+        ./gradlew imgui-binding:generateLibs -Denvs=windows -Dfreetype=true
         if [ $? -ne 0 ]; then
             echo "Gradle task for Windows failed"
             exit 1
@@ -60,7 +60,7 @@ case "$VTYPE" in
         ;;
     linux)
         echo "Running Gradle task for Linux..."
-        ./gradlew imgui-binding:generateLibs -Denvs=linux -Dfreetype=false
+        ./gradlew imgui-binding:generateLibs -Denvs=linux -Dfreetype=true
         if [ $? -ne 0 ]; then
             echo "Gradle task for Linux failed"
             exit 1
